e312aa1a272020c6c4f9c9669baf14a34eb5851a
thaemorais <thaelenml15@gmail.com>
2024-11-06 23:00:02 -0300

    port

--- backend/index.js
+++ backend/index.js
@@ -3,10 +3,11 @@ import cors from "cors";
 import tarefasRoutes from "./routes/tarefas.js";
 
 const app = express();
+const port = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(cors());
 
 app.use("/", tarefasRoutes);
 
-app.listen(process.env.PORT);
+app.listen(port);
