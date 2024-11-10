b0386da8d07f72296edf9d9a628d9dcfcd7f235f
thaemorais <thaelenml15@gmail.com>
2024-11-10 12:53:38 -0300

    banco em nuvem

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,9 @@
-import mysql from "mysql";
-import dotenv from "dotenv";
-
-dotenv.config();
+import mysql from "mysql2";
 
 export const db = mysql.createConnection({
-	host: process.env.HOST,
-	user: process.env.USER,
-	password: process.env.DB_PASSWORD,
-	database: process.env.DATABASE,
+	host: "sql.freedb.tech",
+	user: "freedb_thae_dev",
+	password: "SJAZGsk$2YA?ym$",
+	database: "freedb_todolistDB",
+	port: 3306,
 });
